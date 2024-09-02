2aa0871b86618108c8ea13055bed758bba405691
HunterStile <you@example.com>
2024-09-03 00:29:36 +0200

    Versione con aggiornamento Server

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,4 @@
 // client/src/config.js
-const API_BASE_URL = 'https://fa43-87-17-95-49.ngrok-free.app/api';
+const API_BASE_URL = 'https://1c22-87-17-95-49.ngrok-free.app/api';
 
 export default API_BASE_URL;

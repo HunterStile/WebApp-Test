95f94e64fbe4721a4142f1885947e3ce318d3f5c
HunterStile <luigidellecave7@gmail.com>
2024-11-10 02:08:30 +0100

    Rimozione di api per la risoluzione del bug delle api in pubblico

--- server/app.js
+++ server/app.js
@@ -2,42 +2,49 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const authRoutes = require('./routes/auth'); // Importa le route di autenticazione
-const tcRoutes = require('./routes/tc'); // Importa le rotte per TC
-const cryptoRoutes = require('./routes/crypto'); // Importa le rotte per le criptovalute
+const authRoutes = require('./routes/auth');
+const tcRoutes = require('./routes/tc');
+const cryptoRoutes = require('./routes/crypto');
 const oddsRoutes = require('./routes/odds');
 
 require('dotenv').config();
 const app = express();
-const port = process.env.PORT || 5000; // Utilizza una variabile d'ambiente per la porta
+const port = process.env.PORT || 5000;
+
+// Middleware per il CORS (importante per le API)
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
 
 // Connessione a MongoDB
-//mongoose.connect('mongodb://localhost:27017/webapp', {
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/WebApp-Test', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://mongodb:27017/WebApp-Test', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
 .then(() => console.log('MongoDB connected...'))
 .catch(err => console.error('MongoDB connection error:', err));
 
-// Middleware per analizzare i corpi JSON
+// Middleware
 app.use(bodyParser.json());
-
-// Serve i file statici dalla build di React
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
-// Usa le route di autenticazione
-app.use('/api/auth', authRoutes);
-app.use('/api/tc', tcRoutes);
-app.use('/api/crypto', cryptoRoutes);
-app.use('/api/odds', oddsRoutes);
+// Routes - Rimuovi il prefisso /api poiché viene gestito da nginx
+app.use('/auth', authRoutes);
+app.use('/tc', tcRoutes);
+app.use('/crypto', cryptoRoutes);
+app.use('/odds', oddsRoutes);
 
-// Serve index.html sulla root route e per tutte le altre rotte
+// Catch-all route
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
 
-// Avvio del server
 app.listen(port, '0.0.0.0', () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on port ${port}`);
 });

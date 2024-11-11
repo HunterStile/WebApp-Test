4eec6bea002992778927e7ec6d08c24805de2728
HunterStile <luigidellecave7@gmail.com>
2024-11-12 00:53:41 +0100

    Test del login conflittuale

--- client/src/App.js
+++ client/src/App.js
@@ -27,7 +27,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
-            <Route path="/login" element={<Login />} />
+            <Route path="/login2" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route path="/oddslist" element={<OddsList />} />
             

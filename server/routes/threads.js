6e4b35fd502c130bd0a86c9dd7f0fd4643c43e89
HunterStile <luigidellecave7@gmail.com>
2025-01-26 00:53:52 +0100

    da capire perchè non funziona il sendadminthreadcreation

--- server/routes/threads.js
+++ server/routes/threads.js
@@ -29,13 +29,8 @@ router.post('/', async (req, res) => {
     if (isAdminCreated) {
       const user = await User.findOne({ username: targetUser });
       if (user) {
-        console.log('User:', user);
-        console.log('Thread:', thread);
         try {
-          await emailService.sendAdminThreadCreationEmail(user, {
-            subject: thread.subject,
-            // Aggiungi altri campi necessari se mancanti
-          });
+          await emailService.sendAdminThreadCreationEmail(user, thread);
         } catch (emailError) {
           console.error('Errore invio email di aggiornamento:', emailError);
         }

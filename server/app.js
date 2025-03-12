703ddebb96614b6564f712275f6667b0c7e9035e
HunterStile <luigidellecave7@gmail.com>
2025-03-12 04:41:04 +0100

    aggiornato mongo

--- server/app.js
+++ server/app.js
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
 });
 
 // Connessione a MongoDB
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/igclone', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://mongodb:27017/igclone', {
 })
 .then(() => console.log('MongoDB connected...'))
 .catch(err => console.error('MongoDB connection error:', err));
